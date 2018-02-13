012f6a41c4c0228c45739aeec6b20e19ee361062
Zachary Rosenlund <zrosenlund@mail.greenriver.edu>
2018-02-13 15:27:35 -0500

    working on sending form data to database, updated signup route in index, created html for signup page

--- index.php
+++ index.php
@@ -8,13 +8,42 @@
 //Require the autoload file
 require_once('vendor/autoload.php');
 
+session_start();
+
 //Create an instance of the Base class
 $f3 = Base::instance();
 
 //Define a default route
 $f3->route('GET /', function() {
-    $view = new View;
-    echo $view->render('pages/home.html');
+    $template = new Template();
+    echo $template->render('pages/home.html');
+}
+);
+
+$f3->route('POST /signup', function($f3) {
+    $_SESSION['username'] = $_POST['createUsername'];
+    $_SESSION['password'] = $_POST['createPassword1'];
+    $f3->set('username', $_POST['createUsername']);
+    $f3->set('password', $_POST['createPassword1']);
+
+    require("../../../userdb.php");
+
+    $username = mysqli_real_escape_string($cnxn, $_SESSION['username']);
+    $password = mysqli_real_escape_string($cnxn, $_SESSION['password']);
+
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($cnxn, $sql);
+    if ($row = mysqli_fetch_assoc($result)){
+        $sql = "INSERT INTO users VALUES ($username, $password, 1)";
+        $result = mysqli_query($cnxn, $sql);
+        if(!$result){
+            print("<p>Error:".mysqli_error($cnxn)."</p>");
+        }
+    }
+    
+
+    $template = new Template();
+    echo $template->render('pages/signup.html');
 }
 );
 
---
+++ js/signup.js
@@ -0,0 +1,5 @@
+$('#titleScreen').hide();
+
+$(document).ready(function(){
+    $('#titleScreen').delay("slow").fadeIn("slow");
+});
