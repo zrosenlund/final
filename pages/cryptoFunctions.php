02f75b5ba8cc397b77a20b13bd01771a60358c9c
Sarah Elkhart <selkhart@mail.greenriver.edu>
2018-02-15 20:32:43 -0500

    added binary to letter function

--- pages/cryptoFunctions.php
+++ pages/cryptoFunctions.php
@@ -8,9 +8,8 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 
-echo "Below";
-$results = lettersToNumbers("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z");
-echo "$results yo";
+$results = binaryToLetters("01000101 01001110 01001001 01000111 01001101 01000001");
+echo "$results";
 
 //function that converts numbers to letters
 function numbersToLetters($string)
@@ -43,7 +42,7 @@ function numbersToLetters($string)
 function lettersToNumbers($string)
 {
     //associative array to use as key
-    $alpha = array("A" =>1 , "B" =>2 ,  "C" => 3, "D" =>4 ,  "E"=>5 ,
+    $alpha = array( 1=>"A" ,  2=>"B" , 3=>"C" ,  4=>"D" ,  5=>"E" ,
         6=> "F",  7=> "G",  8=> "H",  9=> "I", 10=> "J",
         11=> "K", 12=> "L", 13=> "M", 14=> "N", 15=> "O",
         16=> "P", 17=> "Q", 18=> "R", 19=> "S", 20=> "T",
@@ -72,18 +71,17 @@ function lettersToNumbers($string)
 function binaryToLetters($string)
 {
     //associative array to use as key
-    $binaryArray = array("E" => 01000101 , "N" => 01001110 ,
-        "I" => 01001001 , "G" => 01000111  ,  "E"=> 01001001  ,
-        6=> "F",  7=> "G",  8=> "H",  9=> "I", 10=> "J",
-        11=> "K", 12=> "L", 13=> "M", 14=> "N", 15=> "O",
-        16=> "P", 17=> "Q", 18=> "R", 19=> "S", 20=> "T",
-        21=> "U", 22=> "V", 23=> "W", 24=> "X", 25=> "Y", 26=> "Z");
+    $binaryArray = array(
+            "A"=> "01000001", "B"=> "01000010", "C"=> "01000011", "D"=> "01000100",
+            "E"=> "01000101", "F"=> "01000110", "G"=> "01000111", "H"=> "01001000",
+            "I"=> "01001001", "J"=> "01001010", "K"=> "01001011", "L"=> "01001100",
+            "M"=> "01001101", "N"=> "01001110", "O"=> "01001111", "P"=> "01010000",
+            "Q"=> "01010001", "R"=> "01010010", "S"=> "01010011", "T"=> "01010100",
+            "U"=> "01010101", "V"=> "01010110", "W"=> "01010111", "X"=> "01011000",
+            "Y"=> "01011001", "Z"=> "01011010" );
 
     //initate variables
     $result = $num = "";
-
-    //make string uppercase
-    $string = strtoupper($string);
 
     //turn string into an array
     $letters = explode(" ", $string, 30);
@@ -91,7 +89,7 @@ function binaryToLetters($string)
     //loop through letters array searching through alpha array and put result into variable
     foreach($letters as $letter)
     {
-        $num = array_search($letter , $alpha);
+        $num = array_search($letter , $binaryArray);
         $result .= $num." ";
     }
     return $result;
