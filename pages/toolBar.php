acd92fc1efb4f858e0f11364362337fe9a6b3edc
Sarah Elkhart <selkhart@mail.greenriver.edu>
2018-02-23 18:24:08 -0500

    added jQuery library tag and submit buttons

--- pages/toolBar.php
+++ pages/toolBar.php
@@ -1,15 +1,29 @@
 
 
 <div id="mySidenav" class="sidenav">
+<!--    letters to numbers-->
     <a href="#">A -> #</a>
         <input class="decode" type="text">
+            <button id="submit"> <-- </button>
+
+<!--    numbers to letters-->
     <a href="#"># -> A</a>
         <input class="decode" type="text">
+    <button id="submit"> <-- </button>
+
+<!--    letter shift-->
     <a href="#">A >> D</a>
         <input class="decode" type="text">
+    <button id="submit"> <-- </button>
+
+<!--    binary to letters-->
     <a href="#">01101101</a>
         <input class="decode" type="text">
+    <button id="submit"> <-- </button>
+
 </div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
 <script>
     $(".decode").hide();
