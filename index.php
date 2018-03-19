fb21066c9da85243c10d75c4b65c5b6ad21d6ae5
Zachary Rosenlund <zrosenlund@mail.greenriver.edu>
2018-03-19 19:54:10 -0400

    added correct index version

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ error_reporting(E_ALL);
 
 //Require the autoload file
 require_once('vendor/autoload.php');
-require("/home/selkhart/config.php");
+require_once('/home/zrosenlu/config.php');
+require_once("classes/dbFunctions.php");
 
 session_start();
 
@@ -28,40 +29,25 @@ $f3->route('GET /', function () {
 
 //****************************************************** SIGN UP *********************************
 $f3->route('POST /signup', function ($f3) {
+
+    require_once('model/validation.php');
+
     $_SESSION['username'] = $_POST['createUsername'];
     $_SESSION['password'] = $_POST['createPassword1'];
-
     $f3->set('username', $_POST['createUsername']);
     $f3->set('password', $_POST['createPassword1']);
-
-    try {
-        //Instantiate a database object
-        $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-        return;
-    }
-
-    //Define the query
-    $sql = "INSERT INTO users(username, password, level)
-           VALUES (:username, :password, :level)";
-
-    //Prepare the statement
-    $statement = $dbh->prepare($sql);
 
     //Bind the parameters
     $username = $_SESSION['username'];
     $password = $_SESSION['password'];
     $level = 1;
-    $statement->bindParam(':username', $username, PDO::PARAM_STR);
-    $statement->bindParam(':password', $password, PDO::PARAM_STR);
-    $statement->bindParam(':level', $level, PDO::PARAM_STR);
 
-    //Execute
-    $statement->execute();
-
-    echo Template::instance()->render("pages/signup.html");
-
+    if (validUsername($username))
+    {
+        $_SESSION['player'] = new Player($username, 1);
+        dbFunctions::insertUser($username, $password, $level);
+        echo Template::instance()->render("pages/signup.html");
+    }
 });
 
 //****************************************************** SIGN UP *********************************
@@ -90,32 +76,50 @@ $f3->route('GET /levels/@pageName', function ($f3, $params) {
 
 //MENU
         case 'menu' :
+            if ($_SESSION['player']->getLevel() < 2) {
+                dbFunctions::setLevel($_SESSION['player']->getName(), 2);
+                $_SESSION['player']->setLevel(2);
+            }
+
+            $f3->set('level', $_SESSION['player']->getLevel());
 
             //set route
-            echo Template::instance()->render('pages//menu.html');
+            echo Template::instance()->render('pages/menu.html');
             break;
 
 //LEVEL 1
-        case 'yak' :
+        case '1' :
             require "pages/toolBar.php";
 
             echo Template::instance()->render('pages/levels/1.php');
             break;
 //LEVEL 2
-        case 'du':
+        case '2':
             require "pages/toolBar.php";
 
             echo Template::instance()->render('pages/levels/2.php');
             break;
 
 //LEVEL 3
-        case 'sey':
+        case '3':
+            if ($_SESSION['player']->getLevel() < 3) {
+                dbFunctions::setLevel($_SESSION['player']->getName(), 3);
+                $_SESSION['player']->setLevel(3);
+            }
+            $f3->set('level', $_SESSION['player']->getLevel());
+
             require "pages/toolBar.php";
 
             echo Template::instance()->render('pages/levels/3.php');
             break;
 //LEVEL 4
-        case 'char':
+        case '4':
+            if ($_SESSION['player']->getLevel() < 4) {
+                dbFunctions::setLevel($_SESSION['player']->getName(), 4);
+                $_SESSION['player']->setLevel(4);
+            }
+            $f3->set('level', $_SESSION['player']->getLevel());
+
             require "pages/toolBar.php";
 
             echo Template::instance()->render('pages/levels/4.php');
@@ -123,6 +127,12 @@ $f3->route('GET /levels/@pageName', function ($f3, $params) {
 
         //LEVEL 5
         case '5':
+            if ($_SESSION['player']->getLevel() < 5) {
+                dbFunctions::setLevel($_SESSION['player']->getName(), 5);
+                $_SESSION['player']->setLevel(5);
+            }
+            $f3->set('level', $_SESSION['player']->getLevel());
+
             require "pages/toolBar.php";
 
             echo Template::instance()->render('pages/levels/5.php');
@@ -130,14 +140,26 @@ $f3->route('GET /levels/@pageName', function ($f3, $params) {
 
 
         //LEVEL 6
-        case 'shash':
+        case '6':
+            if ($_SESSION['player']->getLevel() < 6) {
+                dbFunctions::setLevel($_SESSION['player']->getName(), 6);
+                $_SESSION['player']->setLevel(6);
+            }
+            $f3->set('level', $_SESSION['player']->getLevel());
+
             require "pages/toolBar.php";
 
             echo Template::instance()->render('pages/levels/6.php');
             break;
 
         //LEVEL 7
-        case 'haft':
+        case '7':
+            if ($_SESSION['player']->getLevel() < 7) {
+                dbFunctions::setLevel($_SESSION['player']->getName(), 7);
+                $_SESSION['player']->setLevel(7);
+            }
+            $f3->set('level', $_SESSION['player']->getLevel());
+
             require "pages/toolBar.php";
 
             echo Template::instance()->render('pages/levels/7.php');
